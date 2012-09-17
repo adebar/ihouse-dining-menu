7f3ba97636a03022888038e84ed40c463ea0eb76
Markus Schanta <markus@schanta.at>
2012-09-17 12:18:38 -0400

    Changed URL string to match document URL.

--- src/menu.sh
+++ src/menu.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+#http://residents.ihouse-nyc.org/s/733/images/editor_documents/menus/week_of_september_17_2012.pdf
 #http://residents.ihouse-nyc.org/s/733/images/editor_documents/menu/weekly_menu_december_12_2011.pdf
 
 monday=`date -v-mon "+%B_%-e_%Y"`
 monday=`echo $monday | tr '[A-Z]' '[a-z]'`
 
-url="http://residents.ihouse-nyc.org/s/733/images/editor_documents/menu/weekly_menu_${monday}.pdf"
+url="http://residents.ihouse-nyc.org/s/733/images/editor_documents/menus/week_of_${monday}.pdf"
 
 echo $url
 
